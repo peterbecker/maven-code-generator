72311bdb25efc5575c43a66991017fc127261466
Peter Becker <peter@conceptaconsulting.com>
2012-07-28 19:42:44 +1000

    Step 3: Writing a first file.

--- plugin/src/main/java/maven/code/generator/CodeGeneratorPlugin.java
+++ plugin/src/main/java/maven/code/generator/CodeGeneratorPlugin.java
@@ -3,11 +3,33 @@ package maven.code.generator;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 /**
- * @goal sayhi
+ * @goal generate
  */
 public class CodeGeneratorPlugin extends AbstractMojo {
+
+    /**
+     * Output directory for generated source files.
+     *
+     * @parameter expression="${project.build.directory}/generated-sources/mavencodegen"
+     */
+    private File outputDirectory;
+
     public void execute() throws MojoExecutionException {
-        getLog().info("Hello, world.");
+        if(!outputDirectory.exists()) {
+            outputDirectory.mkdirs();
+        }
+        File testFile = new File(outputDirectory, "test.txt");
+        try {
+            PrintWriter testOut = new PrintWriter(testFile);
+            testOut.println("Hello World.");
+            testOut.close();
+        } catch (FileNotFoundException e) {
+            throw new MojoExecutionException("Can not write to " + testFile.getAbsolutePath(), e);
+        }
     }
 }
