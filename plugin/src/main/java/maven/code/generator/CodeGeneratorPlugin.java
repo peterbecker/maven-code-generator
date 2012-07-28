51a96e3ab3c108e490a092d860ee3a22d82280f0
Peter Becker <peter@conceptaconsulting.com>
2012-07-28 20:26:20 +1000

    Step 4: Generating code via Freemarker.

--- plugin/src/main/java/maven/code/generator/CodeGeneratorPlugin.java
+++ plugin/src/main/java/maven/code/generator/CodeGeneratorPlugin.java
@@ -1,11 +1,14 @@
 package maven.code.generator;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @goal generate
@@ -23,13 +26,22 @@ public class CodeGeneratorPlugin extends AbstractMojo {
         if(!outputDirectory.exists()) {
             outputDirectory.mkdirs();
         }
-        File testFile = new File(outputDirectory, "test.txt");
+
+        Configuration fmConfig = new Configuration();
+        fmConfig.setClassForTemplateLoading(CodeGeneratorPlugin.class, "/templates");
+
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("message", "Hello World.");
+
         try {
-            PrintWriter testOut = new PrintWriter(testFile);
-            testOut.println("Hello World.");
-            testOut.close();
-        } catch (FileNotFoundException e) {
-            throw new MojoExecutionException("Can not write to " + testFile.getAbsolutePath(), e);
+            Template template = fmConfig.getTemplate("HelloWorld.java.ftl");
+            File testFile = new File(outputDirectory, "HelloWorld.java");
+            Writer out = new OutputStreamWriter(new FileOutputStream(testFile));
+            template.process(data, out);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Can not generate code in target location", e);
+        } catch (TemplateException e) {
+            throw new MojoExecutionException("Can not process template", e);
         }
     }
 }
